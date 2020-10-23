f0640495ab8fa7d4508e7543af181638eaea59ae
Jonathan Terhorst <terhorst@gmail.com>
2020-10-22 21:00:17 -0400

    update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0',
+    VERSION: '1.0.4.dev3+g6ae67c3.d20201023',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
