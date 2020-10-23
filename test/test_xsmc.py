a61e6faeee13d883cd7b51bd313d251aed36d658
Jonathan Terhorst <terhorst@gmail.com>
2020-10-23 11:44:32 -0400

    add tests

--- test/test_xsmc.py
+++ test/test_xsmc.py
@@ -19,5 +19,15 @@ def test_sample_paths_1(ts):
 def test_sample_paths_2(ts):
     xsmc.XSMC(ts, 0, [1, 2]).sample(k=100)
 
+
 def test_viterbi(ts):
     xsmc.XSMC(ts, 0, [1, 2]).viterbi()
+
+
+def test_viterbi_100():
+    data = msp.simulate(
+        sample_size=100, length=1e8, mutation_rate=1.4e-8, recombination_rate=1e-9
+    )
+    xs = xsmc.XSMC(data, 0, list(range(1, 100)))
+    N0 = xs.theta / (4 * 1e-8)
+    map_path = xs.viterbi()
